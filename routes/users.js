fbfdadf8b2dccd4eb32f1c274a713873f10a5df4
Motocarota <motocarota@gmail.com>
2022-07-07 12:49:27 +0200

    users data in json file

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+function getUsers() {
+  const buf = fs.readFileSync(__dirname + '/../json/users.json', 'utf8');
+  const json = JSON.parse(buf);
+  return json;
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  res.json([{ id: 1 }, { id: 2 }])
+  // res.send('all users');
+  // res.json([{ id: 1 }, { id: 2 }])
+  const json = getUsers();
+  res.json(json);
 });
+
+router.get('/:id', function (req, res, next) {
+  // res.send('get user ' + req.params.id)
+  const json = getUsers();
+  res.json(json[req.params.id]);
+})
 
 module.exports = router;
